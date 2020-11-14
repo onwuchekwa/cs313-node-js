789ae55c80264ce45779c4374529b8b61cad8b9b
Sunday Onwuchekwa <donsonde@gmail.com>
2020-11-14 13:20:26 +0000

    c

--- week09/modules/rates.js
+++ week09/modules/rates.js
@@ -1,5 +1,22 @@
+/**
+ * RATE MODULE
+ * This is the module for handling API endpoints related to postage rates.
+ * @author  James D. Downer
+ * @version 1.0
+ * @since   November 18, 2018
+ */
+
 module.exports = {
+    /**
+     * GET RATE
+     * This is the function portion of the "/getRate" endpoint.
+     * If an input is invalid, then a simple reponse is sent back with the matching
+     * error code.
+     * @param req The Express HTTP request object (populated)
+     * @param res The Express HTTP response object (to be populated)
+     */
     getRate: (req, res) => {
+        // enforce needed paramters
         if (!('weight' in req.query && 'type' in req.query)) {
             return respond(res, {
                 status: 400,
@@ -10,12 +27,13 @@ module.exports = {
             });
         }
 
+        // check for and set output type
         let outputType = 'text/html';
         if ('output' in req.query) {
             switch (req.query.output) {
                 case 'json':
                     outputType = 'application/json';
-                break;
+                    break;
                 
                 case 'html':
                 case 'ajax':
@@ -23,7 +41,7 @@ module.exports = {
                 case null:
                 case undefined:
                     outputType = 'text/html';
-                break;
+                    break;
 
                 // "output" must be valid or omitted
                 default:
@@ -37,6 +55,7 @@ module.exports = {
             }
         }
 
+        // get values from the URL query
         let weight = Number(req.query.weight);
         let type = req.query.type;
 
@@ -83,110 +102,171 @@ module.exports = {
     },
 };
 
-const mapTypeRate = {
+/**
+ * TYPE RATE MAP
+ * A map to match the package type to the rate calculation function.
+ */
+const typeRateMap = {
     'letter-stamped': rateLetterStamped,
     'letter-metered': rateLetterMetered,
     'large-flat': rateLargeFlat,
-    'first-class-retail': rateFirstClassRetail
-}
+    'first-class-retail': rateFirstClassRetail,
+};
 
-const mapTypeName = {
+/**
+ * TYPE NAME MAP
+ * A map to match the package type with a more human-readable type name.
+ */
+const typeNameMap = {
     'letter-stamped': 'Letter (Stamped)',
     'letter-metered': 'Letter (Metered)',
     'large-flat': 'Large Envelope (Flat)',
-    'first-class-retail': 'First-Class Package Service—Retail'
-}
+    'first-class-retail': 'First-Class Package Service—Retail',
+};
 
-const rateLetterStamped = (weight) => {
-    if(weight <= 1.0) {
-        return 0.55;
-    } else if(weight <= 2.0) {
-        return 0.70;
+/**
+ * RATE : LETTER (STAMPED)
+ * This calculates the rate of a stamped letter or deferes the calculation to
+ * the "First Class Mail" calculator if the weight is over 3.5 oz.
+ * @param weight The weight of the letter
+ * @returns      The applied rate
+ * @throws       An error if the weight is too much
+ */
+function rateLetterStamped(weight) {
+    if (weight <= 1.0) {
+        return 0.50;
+    } else if (weight <= 2.0) {
+        return 0.71;
     } else if (weight <= 3.0) {
-        return 0.85;
+        return 0.92;
     } else if (weight <= 3.5) {
-        return 1.00;
+        return 1.13;
     } else {
         return rateLargeFlat(weight);
     }
 }
 
-const rateLetterMetered = (weight) => {
+/**
+ * RATE : LETTER (METERED)
+ * This calculates the rate of a metered letter or deferes the calculation to
+ * the "First Class Mail" calculator if the weight is over 3.5 oz.
+ * @param weight The weight of the letter
+ * @returns      The applied rate
+ * @throws       An error if the weight is too much
+ */
+function rateLetterMetered(weight) {
     if (weight <= 1.0) {
-        return 0.5;
+        return 0.47;
     } else if (weight <= 2.0) {
-        return 0.65;
+        return 0.68;
     } else if (weight <= 3.0) {
-        return 0.80;
+        return 0.89;
     } else if (weight <= 3.5) {
-        return 0.95;
+        return 1.10;
     } else {
         return rateLargeFlat(weight);
     }
 }
 
-const rateLargeFlat = (weight) => {
+/**
+ * RATE : LARGE ENVELOPE (FLAT)
+ * @param weight The weight of the envelope
+ * @returns      The applied rate
+ * @throws       An error if the weight is greater than 13.0 oz
+ */
+function rateLargeFlat(weight) {
     if (weight <= 1.0) {
         return 1.00;
     } else if (weight <= 2.0) {
-        return 1.20;
+        return 1.21;
     } else if (weight <= 3.0) {
-        return 1.40;
+        return 1.42;
     } else if (weight <= 4.0) {
-        return 1.60;
+        return 1.63;
     } else if (weight <= 5.0) {
-        return 1.80;
+        return 1.84;
     } else if (weight <= 6.0) {
-        return 2.00;
+        return 2.05;
     } else if (weight <= 7.0) {
-        return 2.20;
+        return 2.26;
     } else if (weight <= 8.0) {
-        return 2.40;
+        return 2.47;
     } else if (weight <= 9.0) {
-        return 2.60;
+        return 2.68;
     } else if (weight <= 10.0) {
-        return 2.80;
+        return 2.89;
     } else if (weight <= 11.0) {
-        return 3.00;
+        return 3.10;
     } else if (weight <= 12.0) {
-        return 3.20;
+        return 3.31;
     } else if (weight <= 13.0) {
-        return 3.40;
+        return 3.52;
     } else {
         throw `Invalid weight "${weight}"`;
     }
 }
 
-const rateFirstClassRetail = (weight) => {
+/**
+ * RATE : FIRST-CLASS MAIL (RETAIL)
+ * @param weight The weight of the package
+ * @returns      The applied rate
+ * @throws       An error if the weight is greater than 13.0 oz
+ */
+function rateFirstClassRetail(weight) {
     if (weight <= 4.0) {
-        return 3.80;
+        return 3.50;
     } else if (weight <= 8.0) {
         return 3.75;
-    } else if (weight <= 8.0) {
-        return 4.60;
+    } else if (weight <= 9.0) {
+        return 4.10;
+    } else if (weight <= 10.0) {
+        return 4.45;
+    } else if (weight <= 11.0) {
+        return 4.80;
     } else if (weight <= 12.0) {
-        return 5.30;
+        return 5.15;
     } else if (weight <= 13.0) {
-        return 5.90;
+        return 5.50;
     } else {
         throw `Invalid weight "${weight}"`;
     }
 }
 
-const calculateRate = (weight, type) => {
-    if (!(type in mapTypeRate)) {
+/**
+ * CALCULATE DATA
+ * This calculates and returns the rate, price and human-readable type name.
+ * @param weight The package/letter weight
+ * @param type   The package/letter type
+ * @returns      A data array
+ * @throws       An error message if the weight or type is invalid
+ * 
+ * The returned data array includes the rate, price and type name (in that order).
+ */
+function calculateData(weight, type) {
+    if (!(type in typeRateMap)) {
         throw `Unknown type "${type}"`;
     } else if (isNaN(weight) || weight <= 0) {
         throw `Invalid weight "${weight}"`;
     }
 
-    let getRate = mapTypeRate[type];
+    let getRate = typeRateMap[type];
     let rate = getRate(weight);
-    let type_name = mapTypeName[type];
-    return [rate, rate * weight, type_name];
+    let typeName = typeNameMap[type];
+    return [rate, rate * weight, typeName];
 }
 
-const respond = (res, obj) => {
+/**
+ * RESPOND
+ * This sends a simple response (typically for errors).
+ * @param res The Express HTTP response object
+ * @param obj A key-value map for the response
+ * 
+ * The key-value map includes:
+ * @var status  The HTTP status code
+ * @var headers A key-value map of HTTP headers
+ * @var message The content message
+ */
+function respond(res, obj) {
     res.status(obj.status);
     for (let key in obj.headers) {
         res.set(key, obj.headers[key]);
