db72c573587ddc00dff7f3f2d3bb6845c0a84467
Sunday Onwuchekwa <donsonde@gmail.com>
2020-11-14 13:08:29 +0000

    d

--- week09/index.js
+++ week09/index.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const rates = require('./module/rates');
+
 const PUBLIC_DIR = path.join(__dirname, 'public')
 const VIEWS_DIR = path.join(__dirname, 'views');
+const rates = require('./module/rates');
 
 express()
   .use(express.static(PUBLIC_DIR))
   .set('views', VIEWS_DIR)
   .set('view engine', 'ejs')
-  .get('/week09', (req, res) => { res.sendFile(`${PUBLIC_DIR}/index.html`); })
+  .get('/', (req, res) => { res.sendFile(`${PUBLIC_DIR}/index.html`); })
   .get('/getRate', (req, res) => { rates.getRate(req, res) })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
